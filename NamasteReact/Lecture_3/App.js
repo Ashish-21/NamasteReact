6202b877f937584f20ffb32922003fa8372fd67c
ashish-21 <ashishchandwani21@gmail.com>
2023-04-30 16:24:18 +0530

    React Components

--- NamasteReact/Lecture_3/App.js
+++ NamasteReact/Lecture_3/App.js
@@ -10,5 +10,20 @@ const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 //jsx => react.createElement => ReactElement(js object) => HTMlElement(object)
 const jsxHeading = <h1>Namaste React using JSX</h1>;
 
+const Title = () => <h1>This is Title using Component</h1>;
+
+const Title2 = () => <h1>This Is Title 2</h1>;
+
+//React Component
+const HeadingComponent = () => {
+	return (
+		<div>
+			{Title2()}
+			<Title />
+			<h1>This is React Funtional Component</h1>
+		</div>
+	);
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
